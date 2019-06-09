aad1268405528208331f5af9f6321ee069573c4d
yusufyilmazfr <yusufyilmazfr@gmail.com>
2019-06-10 00:12:17 +0300

    implemented generic repository pattern with Entity Framework

---
+++ SilkPlaster/SilkPlaster.DataAccessLayer/EntityFramework/MyInitializer.cs
@@ -0,0 +1,44 @@
+﻿using SilkPlaster.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SilkPlaster.DataAccessLayer.EntityFramework
+{
+    public class MyInitializer : CreateDatabaseIfNotExists<DatabaseContext>
+    {
+        protected override void Seed(DatabaseContext context)
+        {
+            Member member = new Member()
+            {
+                FirstName = "Yusuf",
+                LastName = "Yılmaz",
+                Email = "yusufyilmazfr@gmail.com",
+                Password = "123",
+                AddedDate = DateTime.Now
+            };
+
+            context.Members.Add(member);
+            context.SaveChanges();
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                Member tempMember = new Member()
+                {
+                    FirstName = FakeData.NameData.GetFirstName(),
+                    LastName = FakeData.NameData.GetSurname(),
+                    Email = FakeData.NetworkData.GetEmail(),
+                    Password = FakeData.NumberData.GetNumber(2656).ToString(),
+                    AddedDate = FakeData.DateTimeData.GetDatetime()
+                };
+
+                context.Members.Add(member);
+            }
+            context.SaveChanges();
+        }
+    }
+}
